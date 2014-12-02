782cda3afed37d59762cd3f95bef109324917b43
Griff Hall <griffhall1@gmail.com>
2014-12-02 13:18:56 -0600

    Initial Commit

---
+++ whats_the_weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  whats_the_weather
+//
+//  Created by Griff Hall on 12/2/14.
+//  Copyright (c) 2014 Griffappolis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
